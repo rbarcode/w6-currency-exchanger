1af51c8e03a6e6c1d97c839bc1c7e9b215c241ac
Richard Barbour <richyb07@gmail.com>
2023-09-08 13:09:47 -0700

    Return from lunch.

--- src/services/exchange-service.js
+++ src/services/exchange-service.js
@@ -4,7 +4,7 @@ export default class ExchangeService {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${base}/${target}/${amount}`);
       const jsonifiedResponse = await response.json();
       if (!response.ok) {
-        const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse["error-type"]}`;
+        const errorMessage = `${response.status} ${response.statusText}: ${jsonifiedResponse["error-type"]}`;
         throw new Error(errorMessage);
       }
       return jsonifiedResponse;
