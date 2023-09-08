d6794e085c2bd5b000b8ad200500a20e378636a0
Richard Barbour <richyb07@gmail.com>
2023-09-08 11:55:02 -0700

    Refactor business logic and ui code for intial app functionality and successfull api call.

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@
 import getExchangeRate from "./js/get-exchange-rate.js";
 
 export function printElements(response, base, target, amount) {
-  document.getElementById("show-response").innerText = `${base} ${amount} = ${target} ${response.conversion_result}. The conversion rate is ${base} 1 = ${target} ${response.conversion_rate}`;
+  document.getElementById("show-response").innerText = `${base} ${amount} = ${target} ${response.conversion_result}. The conversion rate is ${base} 1 = ${target} ${response.conversion_rate}.`;
 }
 
-export function printUnsupportedError(error, base, target) {
-  document.getElementById("show-response").innerText = `${error}. The supplied codes were: ${base}, ${target}.`;
+export function printUnsupportedError(error, message, base, target) {
+  document.getElementById("show-response").innerText = `${error}. ${message} The supplied codes were: ${base}, ${target}.`;
 }
 
 export function printError(error) {
-  document.getElementById("show-response").innerText = `${error}.`
+  document.getElementById("show-response").innerText = `${error}.`;
 }
 
 function handleFormSubmission(event) {
@@ -20,7 +20,7 @@ function handleFormSubmission(event) {
   const amountToConvert = document.getElementById("amount").value;
   const baseCurrency = document.getElementById("base-select").value;
   const targetCurrency = document.getElementById("target-select").value;
-  document.getElementById("amount").value
+  document.getElementById("amount").value;
   getExchangeRate(baseCurrency, targetCurrency, amountToConvert);
 }
 
--- src/js/get-exchange-rate.js
+++ src/js/get-exchange-rate.js
@@ -1,13 +1,13 @@
 import ExchangeService from "../services/exchange-service.js";
-import {printElements, printUnsupportedError, printError} from "../index.js";
+import { printElements, printUnsupportedError, printError } from "../index.js";
 
 export default async function getExchangeRate(base, target, amount) {
   const response = await ExchangeService.getExchangeRate(base, target, amount);
   if (response.result === "success") {
     printElements(response, base, target, amount);
-  } else if (response.error-type === "unsupported-code") {
+  } else if (response["error-type"] === "unsupported-code") {
     const unsupCurrency = "We do not support at least one of the supplied currencies.";
-    printUnsupportedError(response, base, target);
+    printUnsupportedError(response, unsupCurrency, base, target);
   } else {
     printError(response);
   }
